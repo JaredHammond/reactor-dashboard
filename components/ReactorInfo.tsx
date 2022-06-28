6caeb85bc3c9752f3b75f5b4660e05a5544e30e2
Jared Hammond <jared@jaredhammond.me>
2022-06-28 16:55:43 -0400

    Further formatting for data card mockup

--- components/ReactorInfo.tsx
+++ components/ReactorInfo.tsx
@@ -2,28 +2,38 @@ import styled from 'styled-components';
 import { LevelGauge, LevelGaugeProps } from './LevelGauge';
 
 const Card = styled.div`
-    height: 300px;
-    max-width: 400px;
+    height: clamp(150px,65vw,300px);
+    width: clamp(200px, 400px, 92%);
+    margin: auto;
     background: whitesmoke;
-    border-radius: 20px;
+    border-radius: 5px;
     display: grid;
     grid-template-columns: 1fr 1.5fr;
-    grid-template-rows: 3rem 1fr;
+    grid-template-rows: 1fr 3rem;
 `
 const StyledLevelGauge = styled(LevelGauge)`
     width: 85%;
+    height: 100%;
     margin: auto;
     grid-column: 1;
-    grid-row: 2;
+    grid-row: 1;
 `
 const CardTitle = styled.h2`
-    grid-row: 1;
+    grid-row: 2;
     grid-column: 1 / 3;
     text-align: center;
     font-size: 1.5rem;
-    color: green;
+    color: black;
+    align-self: center;
 `
 
+const DataBlock = styled.div`
+    grid-column: 2;
+    grid-row: 1;
+    padding-left: 1rem;
+    padding-top: 1rem;
+    line-height: 0.7rem
+`
 
 const props: LevelGaugeProps = {
     vesselId: 'titan',
@@ -45,7 +55,12 @@ export const ReactorInfo = () => {
         <Card>
             <CardTitle>Titan 2</CardTitle>
             <StyledLevelGauge {...props} />
-            
+            <DataBlock>
+                <h3>Temperature</h3>
+                <span>134.1 °F</span>
+                <h3>Pressure</h3>
+                <span>34.2 psig</span>
+            </DataBlock>
         </Card>
     )
 }
